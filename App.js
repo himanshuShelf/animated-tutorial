3a96862f82962c377d36f76a9580763fb3c11f9e
Himanshu Chhatpar <himanshu@shelfpay.in>
2024-01-14 11:22:01 +0530

    feat: Platform specific code

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Hello from './Hello';
+
+const DATA = [
+  "red",
+  "blue", 
+  "green"
+]
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Hello />
       <StatusBar style="auto" />
     </View>
   );
---
+++ Hello.ios.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native"
+
+function Hello() {
+    return <Text style={{fontSize: 32, fontWeight: "700"}}>IOS</Text>
+}
+
+export default Hello;
